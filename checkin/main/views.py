2af7928cc274f21818893c28c551ec4e311ff57d
Tianshu Huang <thetianshuhuang@gmail.com>
2019-09-10 15:38:21 -0500

    Added guest mode to templates

--- checkin/main/views.py
+++ checkin/main/views.py
@@ -6,9 +6,11 @@ from api.models import Program, UserToken
 
 def main(request):
 
-    return render(
-        request, 'main.html', {
+    context = {"user": request.user}
+    if request.user.is_authenticated:
+        context.update({
             "programs": list(Program.objects.filter(owner=request.user)),
             "tokens": list(UserToken.objects.filter(owner=request.user)),
-            "user": request.user
         })
+
+    return render(request, 'main.html', context)
